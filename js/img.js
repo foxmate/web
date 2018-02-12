3e6739caf30f0f1191776f3739c8b0169657dc62
foxmate <foxmate@gmail.com>
2018-02-13 00:15:44 +0100

    Signed-off-by: foxmate <foxmate@gmail.com>

--- js/img.js
+++ js/img.js
@@ -1,8 +1,8 @@
-function openTab(cityName) {
+function openTab(act_name) {
     var i;
     var x = document.getElementsByClassName("tabs");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
     }
-    document.getElementById(cityName).style.display = "block"; 
+    document.getElementById(act_name).style.display = "block"; 
 }
